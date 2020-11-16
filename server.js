6aa7d129ccf09989ea0045392ba174e718ea1644
Igor Agapov <aio350@yahoo.com>
2020-11-16 12:28:42 +0500

    update

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.use(cors())
 app.use(helmet())
 
 app.use((req, res) => {
+  res.setHeader('X-Frame-Options', 'sameorigin')
+  res.setHeader('Content-Security-Policy', "frame-ancestors 'self';")
   res.setHeader('Accept-Encoding', 'gzip, compress, br')
   res.sendFile(__dirname + decodeURIComponent(req.url))
 })
